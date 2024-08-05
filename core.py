111c4f9fb3b48239e1c2891323001e7ef9e3aed1
soojin_seong <soojin_seong@westworld.co.kr>
2024-08-05 13:23:45 +0900

    flask, ngingx, uwsgi 연동

--- core.py
+++ core.py
@@ -1,5 +1,6 @@
 # :coding: utf-8
 
+import re
 import yaml
 import openai
 
@@ -8,6 +9,10 @@ from langchain_core.prompts import SystemMessagePromptTemplate , HumanMessagePro
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core import output_parsers 
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnablePassthrough
+from langchain.schema import Document
 
 from openpyxl import Workbook
 from openpyxl.drawing.image import Image
@@ -154,7 +159,133 @@ class PreprodAI:
         return chain
 
         
+class PreprodAI_PDF:
+    def __init__(self):
+        with open( './config.yml' ) as f:
+            data = yaml.load( f, Loader = yaml.FullLoader )
+            api_key = data['api_key']
 
+        params = {
+                    'temperature' : 0,
+        }
         
+        self.client = ChatOpenAI(
+                    model = 'gpt-4o-mini',
+                    api_key = api_key,
+                    **params,
+        )
+        
+        self.scene_numbers = list()
 
+    def remove_page_numbers( self, text ):
+        text = re.sub(r'\s*-\s*\d+\s*-\s*', '', text, flags=re.MULTILINE)
+        return text
+
+    def combine_pages_with_overlap( self, pages, pages_per_combination=10, overlap=150 ):
+        combined_pages = []
+        num_pages = len(pages)
+        
+        for i in range(0, num_pages, pages_per_combination):
+            combined_text = ''
+            
+            if i > 0:
+                previous_overlap = pages[i-1][-overlap:]
+                combined_text += previous_overlap + ' '
+            
+            combined_text += ' '.join(pages[i:i + pages_per_combination])
+            
+            if i + pages_per_combination < num_pages:
+                next_overlap = pages[i + pages_per_combination][:overlap]
+                combined_text += ' ' + next_overlap
+            
+            combined_pages.append(combined_text)
+        
+        return combined_pages
+
+    def add_blank_lines_around_numbers( self, text ):
+        pattern = r'(?<!\n)\n(?:\d{0,3}|\#|\s*SCENE\s*|\s*씬\s*|\s*S#\s*|\s*장면\s*)\d+.*?(?=\n|\Z)'
+        replacement = r'\n\g<0>\n'
+        updated_text = re.sub(pattern, replacement, text, flags=re.DOTALL | re.IGNORECASE)
+        return updated_text
+
+    def format_documents( self, docs ):
+        if '\n' not in docs:
+            for doc in docs:
+                combined_text = doc.page_content.replace('. ', '.\n')
+                combined_text = self.add_blank_lines_around_numbers(combined_text)
+        else:
+            combined_text = '\n'.join(doc.page_content for doc in docs)
+        return combined_text
+
+    def parse_scene_results( self, results ):
+        parsed_results = []
+        pattern = re.compile(r'(\d{1,3}),\s*(.+),\s*(.+)', re.DOTALL | re.IGNORECASE)
+        for res in results:
+            lines = res.split('\n')
+            for line in lines:
+                line = line.strip()
+                if line:
+                    line = line.strip('[]')
+                    match = pattern.match(line)
+                    if match:
+                        scene_number = match.group(1).strip()
+                        location = match.group(2).strip()
+                        summary = match.group(3).strip()
+                        if scene_number not in self.scene_numbers:
+                            parsed_results.append([scene_number, location, summary])
+                            self.scene_numbers.append(scene_number)
+        return parsed_results
+
+    def find_location_from_pdf( self, pdf_filepath ):
+        loader = PyPDFLoader(pdf_filepath)
+        pages = loader.load()
+
+        cleaned_pages = [self.remove_page_numbers(page.page_content) for page in pages]
+
+        combined_pages = self.combine_pages_with_overlap(cleaned_pages, pages_per_combination=5, overlap=150)
+
+        cleaned_documents = [
+            Document(page_content=content, metadata=pages[i].metadata)
+            for i, content in enumerate(combined_pages)
+        ]
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=50000, chunk_overlap=200)
+        splits = text_splitter.split_documents(cleaned_documents)
+
+        template = "당신은 시나리오 전문가입니다."
+        template += "다음 시나리오를 바탕으로 각 씬을 다음 형식으로 요약하세요 : [씬 넘버, 메인 장소, 씬 내용 1줄요약]."
+        template += "씬 내용 1줄요약은 '~하는 장면'으로 요약해 주세요. 추가 설명 없이 형식에 맞춰서만 작성해 주세요."
+        template += "응답은 반드시 한국어로 작성하세요."
+        template += " **씬 넘버는 시나리오 내용에 포함된 번호를 반드시 그대로 사용해 주세요.**"
+        template += "아래는 예시입니다:"
+        template += "예시 시나리오:"
+        template += "씬 46: 공원"
+        template += "주인공이 친구와 만나 이야기를 나눈다."
+        template += "씬 47: 카페"
+        template += "주인공이 커피를 마시며 서류를 정리한다."
+        template += "응답 예시:"
+        template += "[46, 공원, 주인공이 친구와 만나 이야기를 나누는 장면]"
+        template += "[47, 카페, 주인공이 커피를 마시며 서류를 정리하는 장면]"
+        template += "내용: {context}"
+
+        prompt = ChatPromptTemplate.from_template(template)
+
+        rag_chain = (
+            {'context': RunnablePassthrough()}
+            | prompt
+            | self.client
+            | StrOutputParser()
+        )
+
+        final_results = []
+        for i in range(len(splits)):
+            results = []
+            chunk_docs = [splits[i]]
+            formatted_documents = self.format_documents(chunk_docs)
+            result = rag_chain.invoke({"context": formatted_documents})
+            results.append(result.strip())
+            parsed_results = self.parse_scene_results(results)
+            final_results.extend(parsed_results)
+
+        return final_results
 
